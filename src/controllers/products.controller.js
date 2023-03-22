1d25e480a59531ab20469ee04f928b09247e20ea
NettoJM <nettojm86@gmail.com>
2023-03-22 20:40:42 -0300

    Add required spaces after "{" and before "}"

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -6,7 +6,7 @@ const HTTP_OK_STATUS = 200;
 const listProducts = async (_req, res) => {
   const { type, message } = await productsService.findAll();
 
-  if (type) return res.status(errorMap.mapError(type)).json({message});
+  if (type) return res.status(errorMap.mapError(type)).json({ message });
 
   res.status(HTTP_OK_STATUS).json(message);
 };
@@ -15,7 +15,7 @@ const getProducts = async (req, res) => {
   const { id } = req.params;
   const { type, message } = await productsService.findById(id);
 
-  if (type) return res.status(errorMap.mapError(type)).json({message});
+  if (type) return res.status(errorMap.mapError(type)).json({ message });
 
   res.status(HTTP_OK_STATUS).json(message);
 };
