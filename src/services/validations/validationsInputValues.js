bbe0bc0fe0b37268140bca1b8c7cc2ed96cd0840
NettoJM <nettojm86@gmail.com>
2023-03-23 17:05:11 -0300

    Add validations for creating a product

--- src/services/validations/validationsInputValues.js
+++ src/services/validations/validationsInputValues.js
@@ -9,7 +9,12 @@ const validateId = (id) => {
 
 const validateNewProduct = (name) => {
   const { error } = productNameSchema.validate(name);
-  if (error) return { type: 'INVALID_VALUE', message: '"name" must be in a valid format' };
+  if (error) {
+    return {
+      type: 'INVALID_VALUE',
+      message: '"name" length must be at least 5 characters long',
+    };
+  }
 
   return { type: null, message: '' };
 };
