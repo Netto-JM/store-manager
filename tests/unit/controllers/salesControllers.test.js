004d3e18d7c19b02da61a38e77d4927c3af18aaf
NettoJM <nettojm86@gmail.com>
2023-04-02 19:29:00 -0300

    Add tests to endpoints that removes a single element by its ID

--- tests/unit/controllers/salesControllers.test.js
+++ tests/unit/controllers/salesControllers.test.js
@@ -104,6 +104,75 @@ describe('Controller unit tests for sales', function () {
     });
   });
 
+  describe('Deleting a single sale', function () {
+    it('should return status 204', async function () {
+      // arrange
+      const res = {};
+      const req = {
+        params: {
+          id: 1,
+        },
+      };
+
+      res.sendStatus = sinon.stub().returns();
+      sinon
+        .stub(salesService, 'deleteById')
+        .resolves(salesServiceMocks.deleteSuccessfulResponse);
+
+      // act
+      await salesController.deleteSale(req, res);
+
+      // assert
+      expect(res.sendStatus).to.have.been.calledWith(204);
+    });
+
+    it('should return an error if an invalid id is passed', async function () {
+      // arrange
+      const res = {};
+      const req = {
+        params: {
+          id: 'abc',
+        },
+      };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(salesService, 'deleteById')
+        .resolves(salesServiceMocks.invalidIdResponse);
+
+      // act
+      await salesController.deleteSale(req, res);
+
+      // assert
+      expect(res.status).to.have.been.calledWith(422);
+      expect(res.json).to.have.been.calledWith({ message: salesServiceMocks.invalidIdResponse.message });
+    });
+
+    it('should return an error if a nonexistent id is passed', async function () {
+      // arrange
+      const res = {};
+      const req = {
+        params: {
+          id: 999,
+        },
+      };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(salesService, 'deleteById')
+        .resolves(salesServiceMocks.notExistingIdResponse);
+
+      // act
+      await salesController.deleteSale(req, res);
+
+      // assert
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.json).to.have.been.calledWith({ message: salesServiceMocks.notExistingIdResponse.message });
+    });
+  });
+
   afterEach(function () {
     sinon.restore();
   });
