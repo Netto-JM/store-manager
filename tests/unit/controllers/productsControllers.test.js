5afbe83558830304038234d837fc44e2beaef6bd
NettoJM <nettojm86@gmail.com>
2023-04-02 18:54:49 -0300

    Add tests to endpoints that retrieve a single element by its ID

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -22,17 +22,89 @@ describe('Controller unit tests for products', function () {
       res.json = sinon.stub().returns();
       sinon
         .stub(productsService, 'findAll')
-        .resolves({
-          type: null,
-          message: productsServiceMocks.allProductsResponse
-        });
+        .resolves(productsServiceMocks.allProductsResponse);
 
       // act
       await productsController.listProducts(req, res);
 
       // assert
       expect(res.status).to.have.been.calledWith(200);
-      expect(res.json).to.have.been.calledWith(productsServiceMocks.allProductsResponse);
+      expect(res.json).to.have.been.calledWith(productsServiceMocks.allProductsResponse.message);
     });
+  });
+
+  describe('Retrieving a single product', function () {
+    it('should return status 200 and the product', async function () {
+      // arrange
+      const res = {};
+      const req = {
+        params: {
+          id: 1,
+        },
+      };
+      
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(productsService, 'findById')
+        .resolves(productsServiceMocks.singleProductResponse);
+
+      // act
+      await productsController.getProduct(req, res);
+
+      // assert
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(productsServiceMocks.singleProductResponse.message);
+    });
+
+    it('should return an error if an invalid id is passed', async function () {
+      // arrange
+      const res = {};
+      const req = {
+        params: {
+          id: 'abc',
+        },
+      };
+      
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(productsService, 'findById')
+        .resolves(productsServiceMocks.invalidIdResponse);
+
+      // act
+      await productsController.getProduct(req, res);
+
+      // assert
+      expect(res.status).to.have.been.calledWith(422);
+      expect(res.json).to.have.been.calledWith({ message: productsServiceMocks.invalidIdResponse.message });
+    });
+
+    it('should return an error if a nonexistent id is passed', async function () {
+      // arrange
+      const res = {};
+      const req = {
+        params: {
+          id: 999,
+        },
+      };
+      
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(productsService, 'findById')
+        .resolves(productsServiceMocks.notExistingIdResponse);
+
+      // act
+      await productsController.getProduct(req, res);
+
+      // assert
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.json).to.have.been.calledWith({ message: productsServiceMocks.notExistingIdResponse.message });
+    });
+  });
+
+  afterEach(function () {
+    sinon.restore();
   });
 });
--- tests/unit/controllers/salesControllers.test.js
+++ tests/unit/controllers/salesControllers.test.js
@@ -22,17 +22,89 @@ describe('Controller unit tests for sales', function () {
       res.json = sinon.stub().returns();
       sinon
         .stub(salesService, 'findAll')
-        .resolves({
-          type: null,
-          message: salesServiceMocks.allSalesResponse
-        });
+        .resolves(salesServiceMocks.allSalesResponse);
 
       // act
       await salesController.listSales(req, res);
 
       // assert
       expect(res.status).to.have.been.calledWith(200);
-      expect(res.json).to.have.been.calledWith(salesServiceMocks.allSalesResponse);
+      expect(res.json).to.have.been.calledWith(salesServiceMocks.allSalesResponse.message);
     });
+  });
+
+  describe('Retrieving a single sale', function () {
+    it('should return status 200 and the sale', async function () {
+      // arrange
+      const res = {};
+      const req = {
+        params: {
+          id: 1,
+        },
+      };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(salesService, 'findById')
+        .resolves(salesServiceMocks.singleSaleResponse);
+
+      // act
+      await salesController.getSale(req, res);
+
+      // assert
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(salesServiceMocks.singleSaleResponse.message);
+    });
+
+    it('should return an error if an invalid id is passed', async function () {
+      // arrange
+      const res = {};
+      const req = {
+        params: {
+          id: 'abc',
+        },
+      };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(salesService, 'findById')
+        .resolves(salesServiceMocks.invalidIdResponse);
+
+      // act
+      await salesController.getSale(req, res);
+
+      // assert
+      expect(res.status).to.have.been.calledWith(422);
+      expect(res.json).to.have.been.calledWith({ message: salesServiceMocks.invalidIdResponse.message });
+    });
+
+    it('should return an error if a nonexistent id is passed', async function () {
+      // arrange
+      const res = {};
+      const req = {
+        params: {
+          id: 999,
+        },
+      };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(salesService, 'findById')
+        .resolves(salesServiceMocks.notExistingIdResponse);
+
+      // act
+      await salesController.getSale(req, res);
+
+      // assert
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.json).to.have.been.calledWith({ message: salesServiceMocks.notExistingIdResponse.message });
+    });
+  });
+
+  afterEach(function () {
+    sinon.restore();
   });
 });
